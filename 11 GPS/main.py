d12dfe787c8389c18151096f5ffc15d0663ca8bc
Petr (Desktop) <petrbohac3@seznam.cz>
2022-11-04 01:59:16 +0100

    jdu chrapat more

--- 11 GPS/main.py
+++ 11 GPS/main.py
@@ -9,12 +9,44 @@ class Graph:
         self._nodes = nodes
         self._matrix = adj_matrix
 
-    def get_neighboring_nodes(self, node):
+    def __get_node_index(self, node):
+        if node not in self._nodes:
+            raise ValueError(f'Unknown node "{node}"')
+        return self._nodes.index(node)
+
+    def __get_node_connections(self, node):
+        return self._matrix[self.__get_node_index(node)]
+
+    def __cost(self, node1, node2) -> int:
+        """
+        :return: the cost of travel between two nodes
+        """
+        return self.__get_node_connections(node1)[self.__get_node_index(node2)]
+
+    def get_neighboring_nodes(self, node) -> list[tuple[any, int]]:
+        """
+        :return: a list of tuples, where each tuple represents a neighboring node => (node, cost)
+        """
         neighbors = []
-        for index, cost in enumerate(self._matrix[self._nodes.index(node)]):
+        for index, cost in enumerate(self.__get_node_connections(node)):
             if cost != 999:
                 neighbors.append((self._nodes[index], cost))
         return neighbors
+
+    def cost(self, *nodes, check_connections=False):
+        """
+        Calculates the cost of travel between all the specified nodes
+        """
+        cost = 0
+        for i, node in enumerate(nodes):
+            if i + 1 == len(nodes):
+                break
+            next_node = nodes[i + 1]
+            intermediary_cost = self.__cost(node, next_node)
+            if intermediary_cost == 999 and check_connections:
+                raise RuntimeError(f'Node "{node}" and "{next_node}" are not directly connected')
+            cost += intermediary_cost
+        return cost
 
 
 CITIES = ("liberec", "ceska-lipa", "chrastava", "new-york", "turnov", "jablonec-nad-nisou")
@@ -39,3 +71,4 @@ distance_graph = Graph(CITIES, DISTANCES)
 time_graph = Graph(CITIES, TIMES)
 
 print(distance_graph.get_neighboring_nodes('liberec'))
+print(distance_graph.cost('liberec', 'turnov', check_connections=True))
